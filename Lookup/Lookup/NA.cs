647ad2ed83fa9f3f6fc8e909220a72fcd0b6e74c
Plutos Fungus <rasmus1070@gmail.com>
2021-10-06 12:21:45 +0200

    Not done yet

---
+++ Lookup/Lookup/NA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lookup
+{
+    public class NA
+    {
+        public string UserName { get; set; }
+        public string Dimension { get; set; }
+        public double PosX { get; set; }
+        public double PosY { get; set; }
+        public double PosZ { get; set; }
+        public NA(string userName, string dimension, double posX, double posY, double posZ)
+        {
+            UserName = userName;
+            Dimension = dimension;
+            PosX = posX;
+            PosY = posY;
+            PosZ = posZ;
+        }
+    }
+}
--- Lookup/Lookup/Program.cs
+++ Lookup/Lookup/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
-
-namespace Lookup
+﻿using Lookup;
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace lookup
 {
-    class Program
+    class Class1
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line;
+            string[] pos;
+            List<NA> thisNACollection = new List<NA>();
+
+            
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\NA.csv");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    pos = line.Split(",");
+                    NA thisNA = new NA(pos[0], pos[1], Convert.ToDouble(pos[2]), Convert.ToDouble(pos[3]), Convert.ToDouble(pos[4]));
+
+                    thisNACollection.Add(thisNA);
+
+
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
